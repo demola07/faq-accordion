c4aaa83f8f66d48c7b641a8358649778392548ef
Ademola Sobaki <demolasobaki@gmail.com>
2020-10-21 20:15:42 +0100

    add javascript code for accordion dropdown

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('Working');
+function initAccordion() {
+	const accordionList = document.querySelectorAll('.faq-menu dt');
+	const activeClass = 'active';
+	console.log(accordionList);
+	if (accordionList.length) {
+		accordionList[0].classList.add(activeClass);
+		accordionList[0].nextElementSibling.classList.add(activeClass);
+
+		function activeAccordion() {
+			this.classList.toggle(activeClass);
+			this.nextElementSibling.classList.toggle(activeClass);
+		}
+
+		accordionList.forEach((item) => {
+			item.addEventListener('click', activeAccordion);
+		});
+	}
+}
+
+initAccordion();
